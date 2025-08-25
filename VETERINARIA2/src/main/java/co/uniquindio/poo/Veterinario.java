91fb05c49d62ad9c6fde2c26174cc398e5f6b78c
santilopez77 <santiago.lopezg1@uqvirtual.edu.co>
2025-08-25 17:06:27 -0500

    final

---
+++ VETERINARIA2/src/main/java/co/uniquindio/poo/Veterinario.java
@@ -0,0 +1,12 @@
+package co.uniquindio.poo;
+
+public class Veterinario {
+    private String nombre;
+    private int cedula;
+
+
+    public Veterinario (String nombre,int cedula){
+        this.nombre=nombre;
+        this.cedula=cedula;
+    }
+}
