7e92489ee50f24211085d4dcefe717ba4c6640c6
santilopez77 <santiago.lopezg1@uqvirtual.edu.co>
2025-08-25 15:08:05 -0500

    update

---
+++ VETERINARIA2/src/main/java/co/uniquindio/poo/Main.java
@@ -0,0 +1,9 @@
+package co.uniquindio.poo;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+//hola mundo
+    }
+}
